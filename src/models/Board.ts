5a3afc178b35151fc7a61cff076c8a1edd344dc8
Roman Markovich <gts-786@mail.ru>
2022-08-30 13:31:44 +0300

    Add method canMove for each figure

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -45,12 +45,12 @@ public getCell(x: number, y: number) {
 }
 
 private addKings() {
-    new King(Colors.BLACK, this.getCell(3, 0))
+    new King(Colors.BLACK, this.getCell(4, 0))
     new King(Colors.WHITE, this.getCell(4, 7))
 }
 
 private addQueens() {
-    new Queen(Colors.BLACK, this.getCell(4, 0))
+    new Queen(Colors.BLACK, this.getCell(3, 0))
     new Queen(Colors.WHITE, this.getCell(3, 7))
 }
 
